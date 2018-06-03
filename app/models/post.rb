158524ebc5d87d1071129c4a8b533fa744708b52
juanjo <jjgeek.sis@gmail.com>
2018-06-03 14:39:46 -0500

    comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   validates_presence_of :title, :body
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517012846) do
+ActiveRecord::Schema.define(version: 20180529183347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 20180517012846) do
     t.string "created_by"
   end
 
+  add_foreign_key "comments", "posts"
 end
